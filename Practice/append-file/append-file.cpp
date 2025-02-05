bd94e7512fbcca7516406a29f33af14f2bddbdd6
SOHEL RANA <sohelrana104322@gmail.com>
2025-02-06 01:05:40 +0600

    complete append- file program

---
+++ Practice/append-file/append-file.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    string sourcePath = "./dir1/text.txt";
+    string targetPath = "./dir2/text2.txt";
+
+    ifstream sourceFile(sourcePath, ios::binary);
+
+    if (!sourceFile)
+    {
+        cerr << "Error: Could not open the source file!" << endl;
+        return 1;
+    }
+
+    ofstream targetFile(targetPath, ios::binary | ios::app);
+
+    if (!targetFile)
+    {
+        cerr << "Error: Could not open the target file!" << endl;
+        return 1;
+    }
+
+    targetFile << sourceFile.rdbuf();
+
+    cout << "File content appended successfully." << endl;
+
+    sourceFile.close();
+    targetFile.close();
+
+    return 0;
+}
